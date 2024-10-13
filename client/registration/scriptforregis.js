80ff8824e3a29c175f1a52946b8215744a520184
ZenXen7 <karlchristian.ajero@cit.edu>
2024-10-13 20:14:05 +0800

    new regis

--- client/registration/scriptforregis.js
+++ client/registration/scriptforregis.js
@@ -1,22 +1,22 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
+// const container = document.getElementById('container');
+// const registerBtn = document.getElementById('register');
+// const loginBtn = document.getElementById('login');
 
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
+// registerBtn.addEventListener('click', () => {
+//     container.classList.add("active");
+// });
 
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
-
-
-const signInButton = document.getElementById('signInButton');
+// loginBtn.addEventListener('click', () => {
+//     container.classList.remove("active");
+// });
 
 
-signInButton.addEventListener('click', function(event) {
-    event.preventDefault();
+// const signInButton = document.getElementById('signInButton');
+
+
+// signInButton.addEventListener('click', function(event) {
+//     event.preventDefault();
     
    
-    window.location.href = '../dashboard.html';
-});
+//     window.location.href = '../dashboard.html';
+// });
