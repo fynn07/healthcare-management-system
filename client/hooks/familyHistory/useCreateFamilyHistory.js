6f1c4b7cd859b1d6ad735a6ad4e5d500295824bd
Fynn <pewdiestuff@gmail.com>
2024-11-02 12:26:05 +0800

    [Client] Implemented Creation and Display of Family History Records

---
+++ client/hooks/familyHistory/useCreateFamilyHistory.js
@@ -0,0 +1,43 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("family-history-form");
+
+    form.addEventListener("submit", async function(event) {
+        event.preventDefault()
+
+        const relationship = document.getElementById("family-history-relationship").value;
+        const condition_illness = document.getElementById("family-history-condition-illness").value;
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get('id');
+
+        try {
+            const token = localStorage.getItem('token');
+            
+            const response = await fetch(`http://127.0.0.1:8000/api/patient/create/${id}/family_history/`, {
+                method: "POST",
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    relationship,
+                    condition_illness,
+                })
+            });
+
+            const data = await response.json();
+            console.log(data)
+
+            if (response.ok) {
+                sessionStorage.setItem('toastMessage', 'Record Successfully Added');
+                sessionStorage.setItem('toastType', 'success');
+                
+            } else {
+                sessionStorage.setItem('toastMessage', 'Failed to Add Record');
+                sessionStorage.setItem('toastType', 'error');
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
+});
--- client/utils/table.js
+++ client/utils/table.js
@@ -2,7 +2,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // const prescriptionTableBody = document.getElementById('prescription-body');
     // const vaccinationTable = document.getElementById('vacc-body');
-    const famTable = document.getElementById('fam-body');
+    // const famTable = document.getElementById('fam-body');
     const socialTable = document.getElementById('social-body');
     const digIdTable = document.getElementById('digId-body');
     
@@ -106,21 +106,21 @@ document.addEventListener('DOMContentLoaded', function () {
     //   vaccinationTable.insertAdjacentHTML('beforeend', row);
     // })
 
-    famData.forEach(item => {
-      const row = `
-        <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-          <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-            ${item.dateAdded}
-          </th>
-          <td class="px-6 py-4">${item.relationship}</td>
-          <td class="px-6 py-4">${item.conditionIllness}</td>
-          <td class="px-6 py-4 text-right">
-            <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
-          </td>
-        </tr>
-      `;
-      famTable.insertAdjacentHTML('beforeend', row);
-    })
+    // famData.forEach(item => {
+    //   const row = `
+    //     <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+    //       <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+    //         ${item.dateAdded}
+    //       </th>
+    //       <td class="px-6 py-4">${item.relationship}</td>
+    //       <td class="px-6 py-4">${item.conditionIllness}</td>
+    //       <td class="px-6 py-4 text-right">
+    //         <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+    //       </td>
+    //     </tr>
+    //   `;
+    //   famTable.insertAdjacentHTML('beforeend', row);
+    // })
 
     socialData.forEach(item => {
       const row = `
