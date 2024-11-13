1f3aff5f2b4cb53a2d67f4dfd4af338050173458
Fynn <pewdiestuff@gmail.com>
2024-11-13 15:12:21 +0800

    [Server] Initialized Vital Sign History Views and Endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -15,9 +15,11 @@ urlpatterns = [
     path("patient/fetch/<int:id>/family_history/", views.fetch_family_history_records),
     path("patient/fetch/<int:id>/social_history/", views.fetch_social_history_records),
     path("patient/fetch/<int:id>/surgical_history/", views.fetch_surgical_history_records),
+    path("patient/fetch/<int:id>/vital_history/", views.fetch_vital_history_records),
     path("patient/create/<int:id>/medication_history/", views.create_medication_history_record),
     path("patient/create/<int:id>/vaccination_history/", views.create_vaccination_history_record),
     path("patient/create/<int:id>/family_history/", views.create_family_history_record),
     path("patient/create/<int:id>/social_history/", views.create_social_history_record),
     path("patient/create/<int:id>/surgical_history/", views.create_surgical_history_record),
+    path("patient/create/<int:id>/vital_history/", views.create_vital_history_record),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import get_object_or_404
 from django.contrib.auth.models import User
 
-from .serializers import UserSerializer, ProviderSerializer, PatientSerializer, MedicationHistorySerializer, VaccinationHistorySerializer, FamilyHistorySerializer, SocialHistorySerializer, SurgicalHistorySerializer
-from .models import Provider, Patient, MedicationHistory, VaccinationHistory, FamilyHistory, SocialHistory, SurgicalHistory
+from .serializers import UserSerializer, ProviderSerializer, PatientSerializer, MedicationHistorySerializer, VaccinationHistorySerializer, FamilyHistorySerializer, SocialHistorySerializer, SurgicalHistorySerializer, VitalHistorySerializer
+from .models import Provider, Patient, MedicationHistory, VaccinationHistory, FamilyHistory, SocialHistory, SurgicalHistory, VitalHistory
 from .pagination import initialize_pagination
 from datetime import date
 
@@ -379,3 +379,52 @@ def fetch_surgical_history_records(request, id):
     return paginator.get_paginated_response(serializer.data)
 
 
+#VITAL SIGN HISTORY
+
+@api_view(['POST'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def create_vital_history_record(request, id):
+    try:
+        provider = Provider.objects.get(account=request.user.id)
+    except Provider.DoesNotExists:
+        return Response({"error" : "Provider not found for this account."}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        patient = Patient.objects.get(id=id, provider=provider)
+    except Patient.DoesNotExist:
+        return Response({"error": "Patient not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    data = request.data.copy()
+    data['patient'] = patient.id
+    data['date_added'] = date.today()
+
+    serializer = VitalHistorySerializer(data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
+
+
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def fetch_vital_history_records(request, id):   
+    try:
+        provider = Provider.objects.get(account=request.user.id)
+    except Provider.DoesNotExist:
+        return Response({"error": "Provider not found for this account."}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        patient = Patient.objects.get(id=id, provider=provider)
+    except Patient.DoesNotExist:
+        return Response({"error": "Patient not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    vital_history = VitalHistory.objects.filter(patient=patient.id)
+
+    paginator, paginated_vital_history = initialize_pagination(vital_history, request)
+
+    serializer = VitalHistorySerializer(paginated_vital_history, many=True)
+    return paginator.get_paginated_response(serializer.data)
