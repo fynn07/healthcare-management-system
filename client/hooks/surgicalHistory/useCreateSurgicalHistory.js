61f7e2a722c311cbf78e452aac6560958c1c2cf7
Fynn <pewdiestuff@gmail.com>
2024-11-13 14:58:09 +0800

    [Client] Created Surgical History Fetch and Create

---
+++ client/hooks/surgicalHistory/useCreateSurgicalHistory.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const form = document.getElementById("surgical-history-form");
+
+    form.addEventListener("submit", async function(event) {
+        event.preventDefault()
+
+        const operation_procedure = document.getElementById("surgical-history-operation-procedure").value;
+        const indication = document.getElementById("surgical-history-indication").value;
+        const hospital = document.getElementById("surgical-history-hospital").value;
+        const operation_date = document.getElementById("surgical-history-operation-date").value;
+
+        const urlParams = new URLSearchParams(window.location.search);
+        const id = urlParams.get('id');
+
+        try {
+            const token = localStorage.getItem('token');
+            
+            const response = await fetch(`http://127.0.0.1:8000/api/patient/create/${id}/surgical_history/`, {
+                method: "POST",
+                headers: {
+                    'Authorization': `Token ${token}`,
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    operation_procedure,
+                    indication,
+                    hospital,
+                    operation_date,
+                })
+            });
+
+            const data = await response.json();
+            console.log(data)
+
+            if (response.ok) {
+                sessionStorage.setItem('toastMessage', 'Record Successfully Added');
+                sessionStorage.setItem('toastType', 'success');
+                
+            } else {
+                sessionStorage.setItem('toastMessage', 'Failed to Add Record');
+                sessionStorage.setItem('toastType', 'error');
+            }
+        } catch (error) {
+            console.error("Error:", error);
+        }
+    });
+});
