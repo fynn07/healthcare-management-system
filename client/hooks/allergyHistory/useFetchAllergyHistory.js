17b5df63dc4f8faa160678ab1461c05417acd2b2
Fynn <pewdiestuff@gmail.com>
2024-11-15 23:22:55 +0800

    [Client] Generate URL parameter on Edit Button Click

--- client/hooks/allergyHistory/useFetchAllergyHistory.js
+++ client/hooks/allergyHistory/useFetchAllergyHistory.js
@@ -115,12 +115,29 @@ function updateAllergyHistoryTable(allergyHistory) {
             <td class="px-6 py-4">${formatted_item.severity}</td>
             <td class="px-6 py-4">${formatted_item.criticality}</td>
             <td class="px-6 py-4 text-right">
-                <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                <p href="#" class="edit-link cursor-pointer font-medium text-blue-600 dark:text-blue-500 hover:underline" data-id="${item.id}">Edit</p>
             </td>
             </tr>
         `;
         allergy_history_body.insertAdjacentHTML('beforeend', row);
     });
+}
+
+// Attach event delegation to the parent element
+document.getElementById('allergy-history-body').addEventListener('click', (event) => {
+    if (event.target.classList.contains('edit-link')) {
+        const itemId = event.target.getAttribute('data-id');
+        updateUrlParameters(itemId);
+    }
+});
+
+function updateUrlParameters(itemId) {
+    const url = new URL(window.location.href);
+    const params = url.searchParams;
+
+    params.set('edit_id', itemId);
+
+    window.history.replaceState({}, '', `${url.pathname}?${params.toString()}`);
 }
 
 // Initial fetch on page load
