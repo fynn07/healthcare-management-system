a090bf9cd5a5f17e5b9397ce815a2e55103c84d0
Fynn <pewdiestuff@gmail.com>
2024-11-16 13:22:31 +0800

    [Server] Implemented Update Vital Sign History Views and Endpoints

--- server/api/urls.py
+++ server/api/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path("patient/fetch/<int:id>/family_history/<int:record_id>/", views.fetch_single_family_history_record),
     path("patient/fetch/<int:id>/social_history/<int:record_id>/", views.fetch_single_social_history_record),
     path("patient/fetch/<int:id>/surgical_history/<int:record_id>/", views.fetch_single_surgical_history_record),
+    path("patient/fetch/<int:id>/vital_history/<int:record_id>/", views.fetch_single_vital_history_record),
     path("patient/fetch/<int:id>/allergy_history/<int:record_id>/", views.fetch_single_allergy_history_record),
     path("patient/create/<int:id>/medication_history/", views.create_medication_history_record),
     path("patient/create/<int:id>/vaccination_history/", views.create_vaccination_history_record),
@@ -35,5 +36,6 @@ urlpatterns = [
     path("patient/update/<int:id>/family_history/<int:record_id>/", views.update_family_history_record),
     path("patient/update/<int:id>/social_history/<int:record_id>/", views.update_social_history_record),
     path("patient/update/<int:id>/surgical_history/<int:record_id>/", views.update_surgical_history_record),
+    path("patient/update/<int:id>/vital_history/<int:record_id>/", views.update_vital_history_record),
     path("patient/update/<int:id>/allergy_history/<int:record_id>/", views.update_allergy_history_record),
 ]
--- server/api/views.py
+++ server/api/views.py
@@ -705,6 +705,61 @@ def fetch_vital_history_records(request, id):
     serializer = VitalHistorySerializer(paginated_vital_history, many=True)
     return paginator.get_paginated_response(serializer.data)
 
+@api_view(['GET'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def fetch_single_vital_history_record(request, id, record_id):
+    try:
+        provider = Provider.objects.get(account=request.user.id)
+    except Provider.DoesNotExist:
+        return Response({"error": "Provider not found for this account."}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        patient = Patient.objects.get(id=id, provider=provider)
+    except Patient.DoesNotExist:
+        return Response({"error": "Patient not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        vital_history = VitalHistory.objects.get(id=record_id, patient=patient)
+    except VitalHistory.DoesNotExist:
+        return Response({"error": "Allergy history record not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    serializer = VitalHistorySerializer(vital_history)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+
+@api_view(['PUT'])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def update_vital_history_record(request, id, record_id):
+    try:
+        provider = Provider.objects.get(account=request.user.id)
+    except Provider.DoesNotExist:
+        return Response({"error": "Provider not found for this account."}, status=status.HTTP_404_NOT_FOUND)
+
+    try:
+        patient = Patient.objects.get(id=id, provider=provider)
+    except Patient.DoesNotExist:
+        return Response({"error": "Patient not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    try:
+        vital_history = VitalHistory.objects.get(id=record_id, patient=patient)
+    except VitalHistory.DoesNotExist:
+        return Response({"error": "Vital history record not found."}, status=status.HTTP_404_NOT_FOUND)
+    
+    # Update the allergy history record with the new data
+    data = request.data.copy()
+    data['patient'] = patient.id  # Ensure the patient ID stays the same
+    data['date_added'] = vital_history.date_added  # Keep the original date_added
+
+    serializer = VitalHistorySerializer(vital_history, data=data)
+
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 #ALLERGY HISTORY 
 
 @api_view(['POST'])
