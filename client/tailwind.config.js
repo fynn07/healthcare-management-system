520d4560857000574266e93e37dbdd9cad3b6cbb
CodexPremiera <ashleyken.professional@gmail.com>
2024-11-30 15:29:50 +0800

    redesign landing banner

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
         // Adds a custom blue shade
         'blue_super_light': '#E1F8FD',
         'blue_main': '#054F99',
+        'blue_light': '#e1effc',
         'blue_cta': '#1B77B6',
         'bookmark-purple': '#6B5B95', // Replace this with your desired color code
         'bookmark-white': '#FFFFFF', // Define 'bookmark-white' if not already present
