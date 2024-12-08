d8127d6319fab66563a1daed903fc272403bf3f9
Fynn <pewdiestuff@gmail.com>
2024-12-08 21:42:18 +0800

    [Client/Server] Implemented Deletion of Patient

--- client/hooks/deletePatient.js
+++ client/hooks/deletePatient.js
@@ -1,22 +1,51 @@
+import { getApiEndpoint } from "../utils/getApiEndpoint.js";
+
+async function deletePatient(){
+    const urlParams = new URLSearchParams(window.location.search);
+    const id = urlParams.get('id');
+
+    const ENDPOINT = getApiEndpoint();
+    
+    try {
+        const token = localStorage.getItem('token');
+        const response = await fetch(`${ENDPOINT}/api/patient/delete/${id}/`, {
+            method: 'PUT',
+            headers: {
+                'Authorization': `Token ${token}`,
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error("Failed to delete patient");
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error deleting patient details:", error);
+        return null;
+    }
+
+}
+
 document.getElementById('delete-patient-button').addEventListener('click', (event) => {
     // Show the modal
     const modal = document.getElementById('confirm-deletion-modal');
 
     modal.classList.remove('hidden');
 
-    // const record_id = new URLSearchParams(window.location.search).get('edit_id');
-
     // Handle confirmation
     document.getElementById('confirm-delete-btn').onclick = async () => {
-        // await deleteSurgicalData(record_id);
+        await deletePatient();
 
-        sessionStorage.setItem('toastMessage', 'Record Successfully Deleted');
+        sessionStorage.setItem('toastMessage', 'Patient Successfully Deleted');
         sessionStorage.setItem('toastType', 'success');
 
-        surgical_form.classList.remove('blur-sm', 'pointer-events-none', 'opacity-50')
         modal.classList.add('hidden');
 
-        location.reload();
+        window.location.href = "/client/pages/digitalid.html";
+
     };
 
     // Handle cancellation
